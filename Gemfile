165dc466fc36ec98e71cf4bf936935a3902dbc27
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-11 10:38:53 -0700

    add everything.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# Gems
+
+source('https://rubygems.org')
+
+gem('sinatra')
+gem('rspec')
+gem('pry')
+gem('sinatra-contrib')
---
+++ lib/album.rb
@@ -0,0 +1,42 @@
+# backend
+
+class Album
+  attr_reader :id, :name
+  attr_accessor :name
+  @@albums = {}
+  @@total_rows = 0
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all()
+    @@albums.values()
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def update(name)
+    @name = name
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
+end
